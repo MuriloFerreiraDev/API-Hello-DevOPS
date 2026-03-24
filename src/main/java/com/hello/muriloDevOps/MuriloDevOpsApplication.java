4c90dabc2aaefb8dbe91e7bfd295ea9caa05c5e3
Murilo Ferreira <muriloout@outlook.com>
2026-03-24 17:12:50 -0300

    first commit

---
+++ src/main/java/com/hello/muriloDevOps/MuriloDevOpsApplication.java
@@ -0,0 +1,13 @@
+package com.hello.muriloDevOps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuriloDevOpsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MuriloDevOpsApplication.class, args);
+	}
+
+}
